1c925433439e3d44f0f0ac67c8593b64ec33d34f
Sahil <sahillakdawala1109@gmail.com>
2023-04-08 03:01:03 +0530

    modified

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,58 @@
-import React, { useState } from 'react'
+import React from 'react'
+// import mode from '../App'
 
-export default function About() {
-    const [myStyle, setmyStyle] = useState({
-        color: 'black',
-        backgroundColor: 'white',
-    })
+export default function About(props) {
+    // const [myStyle, setmyStyle] = useState({
+    //     color: '#1d2633',
+    //     backgroundColor: 'white',
+    // })
 
-    const [btnText, setbtnText] = useState('Enable Dark Mode')
-
-    const toggleStyle = () => {
-        if (myStyle.color === '#1d2633') {
-            setmyStyle({
-                color: 'white',
-                backgroundColor: '#1d2633',
-                // border: '1px solid white'
-            })
-            setbtnText('Enable Light Mode')
-        }
-        else {
-            setmyStyle({
-                color: '#1d2633',
-                backgroundColor: 'white'
-            })
-            setbtnText('Enable Dark Mode')
-        }
+    let myStyle = {
+        color: props.mode === 'dark' ? 'white' : '#1d2633',
+        backgroundColor: props.mode === 'dark' ? '#1d2633' : 'white',
+        borderColor: props.mode === 'dark' ? 'white' : '#1d2633',
+        '.accordion-button.color': props.mode === 'dark' ? 'white !important' : '#1d2633 !important',
+        '.accordion-button.backgroundColor': props.mode === 'dark' ? '#1d2633 !important' : 'white !important'
     }
+
+
+
+    // const changeMode = (props) => {
+    //     if (mode === 'light') {
+    //         setmyStyle({
+    //             color: '#1d2633',
+    //             backgroundColor: 'white',
+
+    //         })
+    //     }
+    //     else {
+    //         setmyStyle({
+    //             color: 'white',
+    //             backgroundColor: '#1d2633',
+    //         })
+    //     }
+    // }
+
+
+    // const [btnText, setbtnText] = useState('Enable Dark Mode')
+
+    // const toggleStyle = () => {
+    //     if (myStyle.color === '#1d2633') {
+    //         setmyStyle({
+    //             color: 'white',
+    //             backgroundColor: '#1d2633',
+    //             // border: '1px solid white'
+    //         })
+    //         setbtnText('Enable Light Mode')
+    //     }
+    //     else {
+    //         setmyStyle({
+    //             color: '#1d2633',
+    //             backgroundColor: 'white'
+    //         })
+    //         setbtnText('Enable Dark Mode')
+    //     }
+    // }
 
     return (
         <div className='container' style={myStyle}>
@@ -33,43 +61,54 @@ export default function About() {
                 <div className="accordion-item">
                     <h2 className="accordion-header">
                         <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style={myStyle}>
-                            Accordion Item #1
+                            <strong>
+                                {/* Browser Compatible */}
+                                Analyse your text
+                            </strong>
                         </button>
                     </h2>
                     <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample" style={myStyle}>
                         <div className="accordion-body">
-                            <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            TextUtils gives you a way to analyze your text quickly and efficiently. Be it word count, character count or
+                            {/* <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow. */}
                         </div>
                     </div>
                 </div>
                 <div className="accordion-item">
                     <h2 className="accordion-header">
                         <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" style={myStyle}>
-                            Accordion Item #2
+                            <strong>
+                                Free to use
+                                {/* Browser Compatible */}
+                            </strong>
                         </button>
                     </h2>
                     <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                         <div className="accordion-body" style={myStyle}>
-                            <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            TextUtils is a free character counter tool that provides instant character count & word count statistics for a given text. TextUtils reports the number of words and characters. Thus it is suitable for writing text with word/ character limit.
+                            {/* <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow. */}
                         </div>
                     </div>
                 </div>
                 <div className="accordion-item">
                     <h2 className="accordion-header">
                         <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" style={myStyle}>
-                            Accordion Item #3
+                            <strong>
+                                Browser Compatible
+                            </strong>
                         </button>
                     </h2>
                     <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample" style={myStyle}>
                         <div className="accordion-body">
-                            <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            This word counter software works in any web browsers such as Chrome, Firefox, Internet Explorer, Safari, Opera. It suits to count characters in facebook, blog, books, excel document, pdf document, essays, etc.
+                            {/* <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow. */}
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="container my-3">
+            {/* <div className="container my-3">
                 <button onClick={toggleStyle} className="btn btn-primary ">{btnText}</button>
-            </div>
+            </div> */}
         </div>
     )
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -41,6 +41,7 @@ export default function TextForm(props) {
         text = textField.value
         textField.select()
         navigator.clipboard.writeText(text);
+        document.getSelection().removeAllRanges()
         props.showAlert('Text copied to clipboard', 'success');
     }
 
@@ -69,21 +70,23 @@ export default function TextForm(props) {
                         {/* <label htmlFor="myBox" className="form-label">Email address</label>
                         <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                         <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div> */}
-                        <textarea className="my-3" name="form-control" value={Text} id="myBox" style={{ backgroundColor: props.mode === 'dark' ? '#1d2633' : 'white', color: props.mode === 'dark' ? 'white' : '#1d2633' }} onChange={handleOnChange} cols="173" rows="8"></textarea>
-                        <button className="btn btn-primary" onClick={handleClearText}>Clear text</button>
-                        <button className="btn btn-primary mx-2 my-2" onClick={handleReverseText}>Reverse text</button>
-                        <button className="btn btn-primary" onClick={handleUpClick}>Convert to Upper case</button>
-                        <button className="btn btn-primary mx-2 my-2" onClick={handleLoClick}>Convert to Lower case</button>
-                        <button className="btn btn-primary" onClick={copyToClipboard}>Copy to clipboard</button>
-                        <button className="btn btn-primary mx-2 my-2" onClick={removeXtraspace}>Remove extra spaces</button>
+                        <textarea className="my-3" name="form-control" value={Text} id="myBox" style={{ backgroundColor: props.mode === 'dark' ? '#1d2633' : 'white', color: props.mode === 'dark' ? 'white' : '#1d2633', width:'100%' }} onChange={handleOnChange} rows="8"></textarea>
+                        <div>
+                        <button disabled={Text.length===0}className="btn btn-primary mx-1 my-1" onClick={handleClearText}>Clear text</button>
+                        <button disabled={Text.length===0}className="btn btn-primary mx-1 my-1" onClick={handleReverseText}>Reverse text</button>
+                        <button disabled={Text.length===0}className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Upper case</button>
+                        <button disabled={Text.length===0}className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lower case</button>
+                        <button disabled={Text.length===0}className="btn btn-primary mx-1 my-1" onClick={copyToClipboard}>Copy to clipboard</button>
+                        <button disabled={Text.length===0}className="btn btn-primary mx-1 my-1" onClick={removeXtraspace}>Remove extra spaces</button>
+                        </div>
                     </div>
                 </form>
             </div>
             <div className="container my-3" style={{ color: props.mode === 'light' ? '#1d2633' : 'white' }}>
                 <h2>Your text summary</h2>
-                <p>total {Text.split(" ").length} words and {Text.length} characters</p>
+                <p>total {Text.split(" ").filter((element)=>{return element.length!==0}).length} words and {Text.length} characters</p>
                 <h3>Preview</h3>
-                <p>{Text.length > 0 ? Text : 'Write something to preview'}</p>
+                <p>{Text.length > 0 ? Text : 'Nothing to preview'}</p>
             </div>
         </>
     )
